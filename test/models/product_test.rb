2240e7381221ae7e695b2b8b089933b456c69738
Anh <anh.tran@employmenthero.com>
2016-07-05 16:07:09 +0700

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products 
   # test "the truth" do
   #   assert true
   # end
+  test "Product attributes must not be empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:image_url].any?
+  	assert product.errors[:price].any?
+
+  end
+
+  def new_product(image_url)
+  	Product.new(title: "My Book Title",
+  		description: "yyy",
+  		price: 1,
+  		image_url: image_url)
+  end
+  test "image url" do
+  	ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+  	bad = %w{fred.doc fred.gif/more fred.gif.more}
+  	ok.each do |name|
+  		assert new_product(name).valid?, "#{name} should be valid"
+  	end
+
+  	bad.each do |name|
+  		assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  	end
+
+  end
+  
+  test "product is not valid without a unique title" do
+  	product = Product.new(title: products(:one).title,
+  		description: "yyy",
+  		price: 1,
+  		image_url: "fred.gif")
+  	assert product.invalid?
+  	assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
